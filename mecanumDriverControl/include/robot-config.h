ba26f9e3a6e29966c6db3a29ce9e6bedc0baf1a1
ClaraGehm <45080706+ClaraGehm@users.noreply.github.com>
2020-06-10 13:24:51 -0400

    Create robot-config.h

---
+++ mecanumDriverControl/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor frontLeft;
+extern motor frontRight;
+extern motor backLeft;
+extern motor backRight;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
