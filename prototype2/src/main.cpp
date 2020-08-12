83dfb7847215db9e5c5bbb49c5737ac32d23f8d3
ClaraGehm <45080706+ClaraGehm@users.noreply.github.com>
2020-08-12 13:40:30 -0400

    Create main.cpp

---
+++ prototype2/src/main.cpp
@@ -0,0 +1,97 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\cjgeh                                            */
+/*    Created:      Mon Jun 29 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// driveLB              motor         1               
+// leftIntake           motor         2               
+// rightIntake          motor         3               
+// indexerMotor         motor         4               
+// flywheel             motor         8               
+// driveRB              motor         10              
+// driveRF              motor         9               
+// driveLF              motor         5               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() 
+{
+  // Initializing Robot Configuration. DO NOT REMOVE!Controller1.Axis3.position(percent) + Controller1.Axis1.position(percent)
+  vexcodeInit();
+  int x = 1;
+  while(true)
+  {
+    //This is for the driving motors
+    driveLB.spin(forward,Controller1.Axis3.value(),percent);
+    driveLF.spin(forward,Controller1.Axis3.value(),percent);
+    driveRB.spin(forward,Controller1.Axis2.value(),percent);
+    driveRF.spin(forward,Controller1.Axis2.value(),percent);
+    
+    //When L1 is pressed the intake moves forward
+    if(Controller1.ButtonL1.pressing())
+    {
+      rightIntake.spin(forward, 12.0,voltageUnits::volt);
+      leftIntake.spin(forward, 12.0, voltageUnits::volt);
+    }
+    else if (Controller1.ButtonL2.pressing())
+    {
+      rightIntake.spin(reverse, 12.0, voltageUnits::volt);
+      leftIntake.spin(reverse, 12.0, voltageUnits::volt);
+    }
+    else
+    {
+      rightIntake.stop(coast);
+      leftIntake.stop(coast);
+    }
+    //When R1 is pressed indexerMotor moves forward
+    if(Controller1.ButtonR1.pressing())
+    {
+      indexerMotor.spin(forward, 12.0, voltageUnits::volt);
+    }
+    //When R2 is pressed indexerMotor moves backwards
+    else if(Controller1.ButtonR2.pressing())
+    {
+      indexerMotor.spin(reverse, 12.0, voltageUnits::volt);
+    }
+    else
+    {
+      indexerMotor.stop(coast);
+    }
+    //When A is pressed the flywheel moves forward
+    if(Controller1.ButtonB.pressing())
+    {
+      if(x==1)
+      {
+     
+        flywheel.spin(reverse,12.0, voltageUnits::volt);
+      }
+      //When B is pressed the flywheel moves backwards
+      else if(x==-1)
+      {
+        flywheel.stop(coast);
+      }
+      else
+      {
+      }
+    }
+    else if(Controller1.ButtonA.pressing())
+    {
+      flywheel.spin(forward,12.0, voltageUnits::volt);
+    }
+    else
+    { 
+    }
+    //Has all motors stop
+  }
+}
