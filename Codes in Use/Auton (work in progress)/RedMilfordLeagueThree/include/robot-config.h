1b56de34554ca3de008b826208865d3693805968
ClaraGehm <45080706+ClaraGehm@users.noreply.github.com>
2021-01-05 18:05:19 -0500

    Create robot-config.h

---
+++ Codes in Use/Auton (work in progress)/RedMilfordLeagueThree/include/robot-config.h
@@ -0,0 +1,40 @@
+using namespace vex;
+
+extern brain Brain;
+
+using signature = vision::signature;
+
+// VEXcode devices
+extern inertial InertialSensor;
+extern encoder leftE;
+extern encoder rightE;
+extern signature eyes__REDB;
+extern signature eyes__BLUEB;
+extern signature eyes__SIG_3;
+extern signature eyes__SIG_4;
+extern signature eyes__SIG_5;
+extern signature eyes__SIG_6;
+extern signature eyes__SIG_7;
+extern vision eyes;
+extern limit BallDetect;
+extern limit BallExit;
+extern controller Controller1;
+extern motor driveLB;
+extern motor leftIntake;
+extern motor rightIntake;
+extern motor indexerMotor;
+extern motor flywheel;
+extern motor driveRB;
+extern motor driveRF;
+extern motor driveLF;
+extern optical OpticalSensor;
+extern distance DistanceSensor;
+extern limit LIntakeLimit;
+extern limit RIntakeLimit;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
