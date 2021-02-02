02e5380ddc33c6d7e4e0fe2e9a406416c42657ad
ClaraGehm <45080706+ClaraGehm@users.noreply.github.com>
2021-02-01 19:36:09 -0500

    Create robot-config.h

---
+++ Codes in Use/Milford Comp/distanceWithPD/include/robot-config.h
@@ -0,0 +1,18 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor driveLB;
+extern distance locator;
+extern motor driveRB;
+extern inertial Inertial3;
+extern motor driveRF;
+extern motor driveLF;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
