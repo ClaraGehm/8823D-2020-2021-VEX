d0d26bccb1541fbc88bfd41eaabe024b9c156c4d
ClaraGehm <45080706+ClaraGehm@users.noreply.github.com>
2021-02-01 19:35:47 -0500

    Create robot-config.cpp

---
+++ Codes in Use/Milford Comp/distanceWithPD/src/robot-config.cpp
@@ -0,0 +1,29 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor driveLB = motor(PORT15, ratio18_1, true);
+distance locator = distance(PORT4);
+motor driveRB = motor(PORT1, ratio6_1, false);
+inertial Inertial3 = inertial(PORT3);
+motor driveRF = motor(PORT9, ratio6_1, false);
+motor driveLF = motor(PORT2, ratio6_1, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
